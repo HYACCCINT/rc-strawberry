aaed3d24ab9c4a4d8b7941e74d3e759cc4c8eea1
Cynthia Wang <hyacccint@gmail.com>
2022-12-21 13:48:05 -0500

    fix: add rc template back

--- hosting/src/App.jsx
+++ hosting/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
